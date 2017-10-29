bff0efc78fb70e4720a8d4ce9265080eb258ef66
Lucas Barros <lbam@cin.ufpe.br>
2017-10-29 20:38:32 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,17 +1,10 @@
-function closeArticle(article){
-	article.css("opacity", 0);
-	setTimeout(function(){
-		article.css("visibility", "hidden")
-	}, 500);
-}
-
 $(".bubble").click(function(){
 	var bubble = $(this)
 	var article = $("#"+bubble.data("article"));
-	article.css({"visibility": "visible", "opacity": 1});
+	article.fadeToggle();
 })
 
 $(".close").click(function(){
 	var article = $(this).parent();
-	closeArticle(article);
+	article.fadeToggle();
 })
